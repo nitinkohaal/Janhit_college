b8533ee710e2a84633e2c29599c07237873ca6b4
nitinkohaal <nitink9915@gmail.com>
2025-05-30 18:00:07 +0530

    commit

--- javascript/script.js
+++ javascript/script.js
@@ -8,24 +8,10 @@ const closeNav = document.getElementById("closeNav");
 
 // color change of navigation scrolll upto 500px down animation 
 
-        
-<<<<<<< HEAD
-    window.addEventListener("scroll",()=>{
-        if(window.scrollY > 100){
-            nav.style.backgroundColor = "#0dcaf0";
-            nav.style.transition = "0.3s";
-            nav.setAttribute("class","navigation  d-flex flex-column gap-1 container-fluid   px-2 pt-1 m-0");
-        }
-        else{
-            nav.style.backgroundColor = "var(--white-shade)";
-            nav.style.transition = "0.5s";
-            nav.setAttribute("class","navigation  d-flex flex-column gap-1 container-fluid   px-2 pt-2 m-0");
-=======
  window.addEventListener("scroll",()=>{
      if(window.scrollY>100){
          gsap.to(nav,{
              backgroundColor:"#04123c"
->>>>>>> 022b1d3b1992cc6fa5c566ab47b51ef285089dfc
 
          })
      }
@@ -35,6 +21,5 @@ const closeNav = document.getElementById("closeNav");
          })
      }
  })
-
 
 
